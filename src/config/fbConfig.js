6964a060cc42b8ca04e6687682f40317204f169e
ztingh <ztingh@gmail.com>
2019-02-16 12:19:31 +0800

    Firebase setting

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,10 +1,17 @@
-import firebase from 'firebase'
+import firebase from 'firebase';
 
+// Put your firebase config in .env file
 var config = {
-// Put your config here
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_PROJECT_ID,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STROAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
 
-firebase.initializeApp(config);
-firebase.firestore().settings({ timestampsInSnapshots: true});
 
-export default firebase
+firebase.initializeApp(config);
+firebase.firestore();
+
+export default firebase;
