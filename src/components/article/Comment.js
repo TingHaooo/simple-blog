932b00747cd5066c553814f0708d28b1a98eb034
Ting-Hao Chang <ztingh@gmail.com>
2019-02-25 18:17:23 +0800

    Modify comment date

--- src/components/article/Comment.js
+++ src/components/article/Comment.js
@@ -4,14 +4,13 @@ import moment from 'moment'
 
 const Comment = (props) => {
   const { comment } = props;
-  console.log(comment);
   return (
     <div className="comment py-3 border-top clearfix">
       <Avatar initial={comment.initial}/>
       <div className="comment-body float-left">
         <h4 className="comment-tile">{comment.title}</h4>
         <div className="user-name text-black-50">{`${comment.authorFirstName} ${comment.authorLastName}`}</div>
-        <div className="date text-black-50">{moment(comment.createAt).format('llll')}</div>
+        <div className="date text-black-50">{moment(comment.createAt.toDate()).format('llll')}</div>
         <div className="comment-content pt-3 border-button text-secondary">{comment.content}</div>
       </div>
     </div>
